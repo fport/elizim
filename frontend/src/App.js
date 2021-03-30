8f80fb76477b1767f2a91a9b7516b2a78b933c72
Furkan Portakal <fportakal@protel.com.tr>
2021-03-30 23:19:47 +0300

    :fire: initialize commit - frontend
    initialize commit - frontend

---
+++ frontend/src/App.js
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="Container">
+        <div>Welcome the Elizim Shop</div>
+    </div>
+  );
+}
+
+export default App;
