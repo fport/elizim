ef9fe8772163cd09779256a00b4b7a900e35e20a
Furkan Portakal <fportakal@protel.com.tr>
2021-04-08 01:05:05 +0300

    added show rating/start

--- frontend/src/components/Product.js
+++ frontend/src/components/Product.js
@@ -1,27 +1,26 @@
 import React from 'react'
 import { Card } from 'react-bootstrap'
+import Rating from './Rating'
 
-const Product = ({product}) => {
+const Product = ({ product }) => {
     return (
         <Card className="my-3 p-3 rounded">
             <a href={`/product/${product._id}`}>
-                <Card.Img src={product.image} variant="top"/>
+                <Card.Img src={product.image} variant="top" />
             </a>
             <Card.Body>
-            <a href={`/product/${product._id}`}>
-                <Card.Title>
-                    <strong>{product.name}</strong>
-                </Card.Title>
-            </a>
+                <a href={`/product/${product._id}`}>
+                    <Card.Title>
+                        <strong>{product.name}</strong>
+                    </Card.Title>
+                </a>
 
-            <Card.Text as="div">
-                <div className="my-3">
-                {product.rating} from {product.numReviews} reviews
-                </div>
-            </Card.Text>
-            <Card.Text as="h3">
-                {product.price}
-            </Card.Text>
+                <Card.Text as="div">
+                    <Rating value={product.rating} text={`${product.numReviews} reviws`} />
+                </Card.Text>
+                <Card.Text as="h3">
+                    {product.price}
+                </Card.Text>
 
             </Card.Body>
         </Card>
