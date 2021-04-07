620bab88fca9ba60188d4163b15419f61c78ecc7
Furkan Portakal <fportakal@protel.com.tr>
2021-04-08 01:11:03 +0300

    fixed error && rating start finished

--- frontend/src/components/Rating.js
+++ frontend/src/components/Rating.js
@@ -69,7 +69,7 @@ const Rating = ({ value, text, color }) => {
 }
 
 Rating.defaultProps = {
-    color: '#f8e825',
+    color: '#db9129',
 }
 
 export default Rating
--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -9,7 +9,7 @@ const HomeScreen = () => {
             <h1>Latest Product</h1>
             <Row>
                 {products.map(product => (
-                    <Col sm={12} lg={4} xl={3}>
+                    <Col key={product._id} sm={12} lg={4} xl={3}>
                         <Product product={product} />
                     </Col>
                 ))}
