473430f5dfa6bc2eee2a7de9017c6eac43f55add
Furkan Portakal <furkanportakalx@gmail.com>
2021-04-18 21:02:02 +0300

    data seeder script

---
+++ backend/data/users.js
@@ -0,0 +1,22 @@
+import bcrypt from 'bcryptjs'
+
+const users = [
+  {
+    name: 'Admin User',
+    email: 'admin@example.com',
+    password: bcrypt.hashSync('123456', 10),
+    isAdmin: true,
+  },
+  {
+    name: 'John Doe',
+    email: 'john@example.com',
+    password: bcrypt.hashSync('123456', 10),
+  },
+  {
+    name: 'Osman Abi',
+    email: 'osman@example.com',
+    password: bcrypt.hashSync('123456', 10),
+  },
+]
+
+export default users
