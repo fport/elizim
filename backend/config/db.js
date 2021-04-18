0c94707e9f1260db0fa3fef76bedc46a1b7dab76
Furkan Portakal <furkanportakalx@gmail.com>
2021-04-18 10:28:45 +0300

    connecting to the database

---
+++ backend/config/db.js
@@ -0,0 +1,18 @@
+import mongoose from 'mongoose'
+
+const connectDB = async () => {
+    try {
+        const conn = await mongoose.connect(process.env.MONGO_URI, {
+            useUnifiedTopology: true,
+            useNewUrlParser: true,
+            useCreateIndex: true,
+        })
+
+        console.log(`MongoDB Connect : ${conn.connection.host}`)
+    } catch (error) {
+        console.log(`Error ${error.message}`)
+        process.exit(1)
+    }
+}
+
+export default connectDB
