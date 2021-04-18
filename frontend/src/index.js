48ae12c74ea1f7aabfe3a74968231551fedf4989
Furkan Portakal <furkanportakalx@gmail.com>
2021-04-19 01:03:18 +0300

    added store && redux devtool

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,14 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux'
+import store from './store'
 import App from './App';
 import './index.css'
 import reportWebVitals from './reportWebVitals';
 import './bootstrap.min.css'
 
 ReactDOM.render(
-  <React.StrictMode>
+  <Provider store={store}>
     <App />
-  </React.StrictMode>,
+  </Provider>,
   document.getElementById('root')
 );
 
