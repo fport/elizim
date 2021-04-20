ea84070fe3a8113c4826b6e0a0ac4fe69ce33204
Furkan Portakal <furkanportakalx@gmail.com>
2021-04-20 23:54:28 +0300

    added cart screen

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -13,7 +13,7 @@ const Header = () => {
           <Navbar.Toggle aria-controls="basic-navbar-nav" />
           <Navbar.Collapse id="basic-navbar-nav">
             <Nav className="ml-auto">
-              <LinkContainer to="/card">
+              <LinkContainer to="/cart">
                 <Nav.Link>
                   <i className="fas fa-shopping-cart"></i> Cart
                 </Nav.Link>
---
+++ frontend/src/screens/CartScreen.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+const CartScreen = () => {
+  return <div>cart screen</div>
+}
+
+export default CartScreen
